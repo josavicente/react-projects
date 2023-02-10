5264a2b27e2afe56d82c4db192d3f6d8b52d6744
Josa Vicente <jvicenpe@gmail.com>
2023-02-10 22:47:29 +0100

    Controlando el submit, tres versiones

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,17 +1,47 @@
+import { useState } from 'react'
 import './App.css'
-
+// import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies: mappedMovies } = useMovies()
+  const [query, setQuery] = useState('')
+
+  // const inputRef = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // Unicampo
+    // Con useRef
+    // const { value } = inputRef.current
+    // console.log(value)
+    // Con FormData un solo campo
+    // const fields = new FormData(event.target)
+    // const query = fields.get('query')
+
+    // Multicampos
+    // const { query } = Object.fromEntries(new window.FormData(event.target))
+    console.log(query)
+  }
+
+  const handleChange = (event) => {
+    // Controlar el formulario viendo como cambia el estado
+    setQuery(event.target.value)
+  }
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form className="form">
-          <input placeholder="Avengers, Star Wars, ..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            value={query}
+            name="query"
+            onChange={handleChange}
+            // ref={inputRef}
+            placeholder="Avengers, Star Wars, ..."
+          />
           <button type="submit">Buscar</button>
         </form>
       </header>
