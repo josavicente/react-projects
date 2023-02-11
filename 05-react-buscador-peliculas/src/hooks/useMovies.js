9f16e0ed9f4868e8e430581eed838dd4746a612c
Josa Vicente <jvicenpe@gmail.com>
2023-02-11 21:32:03 +0100

    Fetch

--- 05-react-buscador-peliculas/src/hooks/useMovies.js
+++ 05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,29 +1,15 @@
 // import withResults from '../mocks/results_ok.json'
 // eslint-disable-next-line no-unused-vars
-import withoutResults from '../mocks/results_ko.json'
+// import withoutResults from '../mocks/results_ko.json'
 import { useState } from 'react'
+import { searchMovies } from '../services/movies'
 
 export function useMovies({ search }) {
-  const [responseMovies, setResponseMovies] = useState([])
+  const [movies, setMovies] = useState([])
 
-  const movies = responseMovies.Search
-
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }))
-
-  const getMovies = () => {
-    if (search) {
-      fetch(`https://www.omdbapi.com/?apikey=3a5bb1d&s=${search}`)
-        .then((response) => response.json())
-        .then((data) => setResponseMovies(data))
-      // setResponseMovies(withResults)
-    } else {
-      setResponseMovies(withoutResults)
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search })
+    setMovies(newMovies)
   }
-  return { movies: mappedMovies, getMovies }
+  return { movies, getMovies }
 }
